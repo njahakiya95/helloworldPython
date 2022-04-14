83d0727a1054c4caffac1bfd2e590c0d8dbfdf4a
Nirmal Patel <patelnirmal1325@gmail.com>
2022-04-13 23:26:44 -0400

    updated calculator.oy

--- calculator.py
+++ calculator.py
@@ -3,3 +3,20 @@ Python Calculator
 
 """
 
+class pyCalculator:
+    #define class constructor with two input variables 
+    def __init__(self, x, y):
+        self.x = x 
+        self.y = y 
+
+    def add(self):
+        return (self.x + self.y)
+    
+    def sub(self):
+        return (self.x - self.y)
+    
+    def mult(self):
+        return (self.x * self.y)
+    
+    def div(self):
+        return (self.x / self.y) 
