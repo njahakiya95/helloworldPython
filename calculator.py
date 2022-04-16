7a33fdc20dffe90b35bb1b8c6d479a48d52ab6b8
Nirmal Patel <patelnirmal1325@gmail.com>
2022-04-16 00:13:53 -0400

    updated calculator.py menu

--- calculator.py
+++ calculator.py
@@ -31,3 +31,32 @@ class Calculator:
 #first and second store the two user input integer numbers respectively
 first = int(input("Enter the first number(MUST be an integer!): "))
 second = int(input("Enter the second number(MUST be an integer!): "))
+
+#create a Calculator object using user input integer numbers 
+user_numbers = Calculator(first, second)
+
+#initialize choice to equal 0 to ensure loop runs at least once
+choice = 0  
+
+#print while loop until user enters a five
+while (choice != 5):
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplicaiton")
+    print("4. Division")
+    print("5. Exiting calculator.py")
+
+    choice = int(input("Enter choice: "))
+    
+    if choice == 1:
+        print("The addition of both numbers equals: ", user_numbers.addition())
+    elif choice == 2:
+        print("The subtraction of both numbers equals: ", user_numbers.subtraction())
+    elif choice == 3:
+        print("The multiplication of both numbers equals: ", user_numbers.multiplication())
+    elif choice == 4:
+        print("The division of both numbers equals: ", user_numbers.division())
+    elif choice == 0:
+        print("Exiting!")
+    else:
+        print("Invalid choice!!")
