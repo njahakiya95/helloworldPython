b26cfa9a156e2acb5d6eea20e36c346fe556773e
Nirmal Patel <patelnirmal1325@gmail.com>
2022-04-16 23:55:50 -0400

    modified try-except block

--- calculator.py
+++ calculator.py
@@ -38,9 +38,9 @@ second = int(input("Enter the second number(MUST be an integer!): "))
 user_numbers = Calculator(first, second)
 
 #initialize choice to equal 0 to ensure loop runs at least once
-choice = 0  
+choice = 0 
 
-#print while loop until user enters a five
+#print while loop with menu of operator options 
 while (choice != 5):
     print("\n1. Addition")
     print("2. Subtraction")
@@ -48,11 +48,13 @@ while (choice != 5):
     print("4. Division")
     print("5. Exit\n")
 
+    #try-except user input and store valid option in choice 
     try:
         choice = int(input("Enter the operation you would like to perform: "))
     except ValueError:
         print("ERROR: Invalid choice! Input must be integer between 1 and 5.\n")
 
+    #if-else loop performs calculations based on the value of choice 
     if choice == 1:
         print("\nThe addition of both numbers equals: ", user_numbers.addition())
     elif choice == 2:
